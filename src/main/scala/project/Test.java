64286e2e7429db03424f21b619a422ba88baf3c9
msharma <mohit.sharma@zalando.de>
2021-05-13 22:14:02 +0200

    adding new test method

--- src/main/scala/project/Test.java
+++ src/main/scala/project/Test.java
@@ -2,7 +2,13 @@ package project;
 
 public class Test {
 
+    public static void testMethod(){
+        System.out.println("test method");
+
+    }
+
     public static void main(String[] args) {
+        testMethod();
         System.out.println("Hello world");
     }
 }
